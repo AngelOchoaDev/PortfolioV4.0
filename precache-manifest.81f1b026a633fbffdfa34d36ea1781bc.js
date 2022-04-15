bbefab891090b96108ddcc7bb7982e864f0802ac
AngelOchoaDev <ayog95@gmail.com>
2022-04-14 22:38:14 -0500

    Updates

---
+++ precache-manifest.81f1b026a633fbffdfa34d36ea1781bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb408c3fd3ba9af41fd4",
+    "url": "/PortfolioV4.0/static/css/main.177e912c.chunk.css"
+  },
+  {
+    "revision": "cb408c3fd3ba9af41fd4",
+    "url": "/PortfolioV4.0/static/js/main.cb408c3f.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/PortfolioV4.0/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "dd0fea094f0ef2dd629f",
+    "url": "/PortfolioV4.0/static/js/2.dd0fea09.chunk.js"
+  },
+  {
+    "revision": "a748120f9b8c5951d6d6",
+    "url": "/PortfolioV4.0/static/js/runtime~main.a748120f.js"
+  },
+  {
+    "revision": "ab65db262e9e6ae5877312f9daa61495",
+    "url": "/PortfolioV4.0/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4523884efd74e993c78a2fb03da03a8.js"
+  "/PortfolioV4.0/precache-manifest.81f1b026a633fbffdfa34d36ea1781bc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/PortfolioV4.0/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
