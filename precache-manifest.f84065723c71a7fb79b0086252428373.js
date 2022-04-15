5c92b3e3914698443615b4e9f7084974e2feff5f
AngelOchoaDev <ayog95@gmail.com>
2022-04-14 19:59:16 -0500

    Updates

---
+++ precache-manifest.f84065723c71a7fb79b0086252428373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb408c3fd3ba9af41fd4",
+    "url": "/PortfolioV3.0/static/css/main.177e912c.chunk.css"
+  },
+  {
+    "revision": "cb408c3fd3ba9af41fd4",
+    "url": "/PortfolioV3.0/static/js/main.cb408c3f.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/PortfolioV3.0/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "dd0fea094f0ef2dd629f",
+    "url": "/PortfolioV3.0/static/js/2.dd0fea09.chunk.js"
+  },
+  {
+    "revision": "04ebd83727e161b7527c",
+    "url": "/PortfolioV3.0/static/js/runtime~main.04ebd837.js"
+  },
+  {
+    "revision": "41b6a6259897afecf3b574eba5a81932",
+    "url": "/PortfolioV3.0/index.html"
+  }
+];
